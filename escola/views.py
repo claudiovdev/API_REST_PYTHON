bb0dbeda48b79c3ffc1459e7ee0a5914ac4d26ea
claudiovdev <c.viniciusmagalhaes@gmail.com>
2023-10-26 08:56:59 -0300

    Adicionando a entidade de matriculas

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from escola.models import Aluno, Curso
-from escola.serializer  import AlunoSerializer,CursoSerializer
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer  import AlunoSerializer,CursoSerializer, MatriculaSerializer
 
 
 class AlunosViewsSet(viewsets.ModelViewSet):
@@ -12,3 +12,8 @@ class CursosViewsSet(viewsets.ModelViewSet):
     """Exibindo todos os cursos"""
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+
+class MatriculaViewsSet(viewsets.ModelViewSet):
+    """Exibindo todos os matriculas"""
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
--- setup/urls.py
+++ setup/urls.py
@@ -1,12 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from escola.views import AlunosViewsSet, CursosViewsSet
+from escola.views import AlunosViewsSet, CursosViewsSet, MatriculaViewsSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('alunos', AlunosViewsSet, basename='Alunos')
 router.register('cursos', CursosViewsSet, basename='Cursos')
+router.register('matriculas', MatriculaViewsSet, basename='Matriculas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
